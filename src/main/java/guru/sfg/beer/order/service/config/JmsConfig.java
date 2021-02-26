51874a5f476c11276aeb702ce79ae2ff519bad85
Wolodja <wolodjawaskovych@i.ua>
2021-02-26 17:58:50 +0100

    Handling Allocation Response. Closes #24

--- src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -6,13 +6,16 @@ import org.springframework.jms.support.converter.MappingJackson2MessageConverter
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
-
 @Configuration
 public class JmsConfig {
 
     public static final String VALIDATE_ORDER_QUEUE = "validate-order";
+
     public static final String VALIDATE_ORDER_RESPONSE_QUEUE = "validate-order-response";
+
     public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
+
+    public static final String ALLOCATE_ORDER_RESPONSE_QUEUE = "allocate-order-response";
 
     @Bean
     public MessageConverter jacksonJmsMessageConverter() {
--- src/main/java/guru/sfg/brewery/model/BeerOrderLineDto.java
+++ src/main/java/guru/sfg/brewery/model/BeerOrderLineDto.java
@@ -40,18 +40,25 @@ public class BeerOrderLineDto {
     @JsonProperty("version")
     private Integer version = null;
 
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @JsonProperty("createdDate")
     private OffsetDateTime createdDate = null;
 
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @JsonProperty("lastModifiedDate")
     private OffsetDateTime lastModifiedDate = null;
 
     private String upc;
+
     private String beerName;
+
     private String beerStyle;
+
     private UUID beerId;
+
     private Integer orderQuantity = 0;
+
     private BigDecimal price;
+
+    private Integer quantityAllocated;
 }
