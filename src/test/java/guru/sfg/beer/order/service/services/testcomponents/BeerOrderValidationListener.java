ff629a6f45ff21c359fc72a0203d9694500736f3
Wolodja <wolodjawaskovych@i.ua>
2021-03-01 17:14:44 +0100

    Added Test for Failed Validation. Closes #28

--- src/test/java/guru/sfg/beer/order/service/services/testcomponents/BeerOrderValidationListener.java
+++ src/test/java/guru/sfg/beer/order/service/services/testcomponents/BeerOrderValidationListener.java
@@ -18,16 +18,22 @@ public class BeerOrderValidationListener {
     private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.VALIDATE_ORDER_QUEUE)
-    public void listen(Message msg){
+    public void listen(Message msg) {
+        boolean isValid = true;
 
         ValidateOrderRequest request = (ValidateOrderRequest) msg.getPayload();
 
         System.out.println("############ I RAN Validation ###########");
 
+        //condition to fail validation
+        if ("fail-validation".equals(request.getBeerOrderDto().getCustomerRef())) {
+            isValid = false;
+        }
+
         jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE,
             ValidateOrderResult.builder()
-            .isValid(true)
-            .orderId(request.getBeerOrderDto().getId())
-            .build());
+                .isValid(isValid)
+                .orderId(request.getBeerOrderDto().getId())
+                .build());
     }
 }
